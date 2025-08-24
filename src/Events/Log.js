147ca8983debd5460027ac2f094ca2c5dc1e98d4
Bouhaik anes mohammed el amine <78903844+S1nju@users.noreply.github.com>
2025-08-24 15:58:49 +0100

    Update message sent to Discord channel

--- src/Events/Log.js
+++ src/Events/Log.js
@@ -19,7 +19,7 @@ module.exports = {
                      if(guild.name ==='Nakhcha <3'){
                       guild.channels.fetch('1010276591303139530').then(channel => {
                         if(channel){
-                          channel.send('@everyone fadi7at zoubir https://cdn.discordapp.com/attachments/1010276591303139530/1404576901191827536/image.png?ex=68a1a06d&is=68a04eed&hm=3b98a5946a062cc997ea2a6fb61ba3337518e6256a14c8036f6b55c1e5e092d8&');
+                          channel.send('@everyone https://cdn.discordapp.com/attachments/1010276591303139530/1408891710485954681/Recording_9.m4a?ex=68ac0ce7&is=68aabb67&hm=f86a0617f263fbe4b93f63cf6224f3892e49630e2a97a549e3dc2d7e070dfe4e&');
                         }
                      });  
                      }
